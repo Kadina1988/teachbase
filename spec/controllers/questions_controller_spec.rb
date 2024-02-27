8bf43f487c0a4bcc01e654c88c617ea12e303112
Kadina1988 <kadina.aziev@mail.ru>
2024-02-27 22:51:57 +0300

    delete before action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,17 +1,118 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
     before { get :index }
 
     it 'populates an array of all questions' do
-
       expect(assigns(:questions)).to match_array(questions)
     end
+
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question.id } }
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+
+    before { get :edit, params: { id: question.id } }
+
+    it 'renders show view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attr' do
+      it 'saves a new quest in the database' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).to be(1)
+      end
+
+      it 'redirect to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attr' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+
+      it 'render new' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'valid attr' do
+      before { patch :update, params: { id: question.id, question: attributes_for(:question) } }
+
+      it 'assigns the requested question to @question' do
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'changes question attributes' do
+        patch :update, params: { id: question.id, question: { title: 'new', body: 'new'} }
+        question.reload
+
+        expect(question.title).to eq('new')
+        expect(question.body).to eq('new')
+      end
+
+      it 'redirect to updated question' do
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'invalid attr' do
+      it 'does not change question' do
+        patch :update, params: { id: question, question: attributes_for(:question, :invalid) }
+        question.reload
+
+        expect(question.title).to eq('MyString')
+        expect(question.body).to eq('MyText')
+      end
+
+      it 'render edit' do
+        patch :update, params: { id: question, question: attributes_for(:question, :invalid) }
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+
+    it 'deletes the question' do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirect ' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to questions_path
     end
   end
 

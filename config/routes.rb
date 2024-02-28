f633c6f95fdcd1cbfbc986dcc4a37ce2c062c682
Kadina1988 <kadina.aziev@mail.ru>
2024-02-28 21:18:07 +0300

    webpack problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :questions, shallow: true do
     resources :answers, only: [:create]
   end
+
+  root to: 'questions#index'
 
 end

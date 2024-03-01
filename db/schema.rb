951526330a24befde6b9fb80969b722b1d3f6e9e
Kadina1988 <kadina.aziev@mail.ru>
2024-03-01 15:07:52 +0300

    created migration user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_28_171843) do
+ActiveRecord::Schema.define(version: 2024_03_01_120616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2024_02_28_171843) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2024_02_28_171843) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "users"
 end
